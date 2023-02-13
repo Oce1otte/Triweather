6d8c1afd5ef8151acf13ca8ee8e0dfe3f96d2160
Yehor Marichev <119524131+YehorMarichev@users.noreply.github.com>
2023-02-13 15:42:52 +0000

    Script for tab switching added

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -8,6 +8,37 @@ export class Controller {
     this._getParams = 'undefined';
   }
 
+  switchTab() {
+    let today = $('#today-tab');
+    let forecast = $('#forecast-tab');
+    
+    function toggleOn(button) {
+      button.css({
+        'pointer-events': 'none',
+        'border-left-color': 'rgb(90, 88, 94)',
+        'border-right-color': 'rgb(90, 88, 94)',
+        'border-bottom-color': 'rgb(235, 77, 103)',
+      });
+    }
+
+    function toggleOff(button) {
+      button.css({
+        'pointer-events': 'auto',
+        'border-left-color': 'transparent',
+        'border-right-color': 'transparent',
+        'border-bottom-color': 'transparent'
+      });
+    }
+
+    if (today.css('pointer-events') === 'none') {
+      toggleOff(today);
+      toggleOn(forecast);
+    } else {
+      toggleOff(forecast);
+      toggleOn(today);
+    }
+  }
+
   getData() {
     this._getParams = `?units=metric&q=${this._cityName}&appid=${this._apiKey}`;
     let queryCurrentUrl = this._apiCurrentUrl + this._getParams;
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,15 @@ $(document).ready(() => {
   const controller = new Controller();
 
   controller.getData();
+  
+  $('#today-tab').click(() => {
+    controller.switchTab();
+    document.title = 'Triweather - Today';
+  });
+
+  $('#forecast-tab').click(() => {
+    controller.switchTab();
+    document.title = 'Triweather - 5-day forecast';
+  });
 
 });
