073919b72c23715db0dcb8195199553e994880b0
Yehor Marichev <119524131+YehorMarichev@users.noreply.github.com>
2023-02-23 03:23:36 +0000

    Data retrieval based on location added

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -1,11 +1,11 @@
 export class Controller {
-  
+
+  // public:
   constructor() {
-    this._apiKey = '995f2b66e783fb11b10fb5cea5c732e8';
-    this._apiCurrentUrl = 'https://api.openweathermap.org/data/2.5/weather';
-    this._apiForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
-    this._cityName = 'Kyiv';
-    this._getParams = 'undefined';
+    this.#apiKey = '995f2b66e783fb11b10fb5cea5c732e8';
+    this.#apiCurrentUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    this.#apiForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+    this.#getParams = 'undefined';
   }
 
   switchTab() {
@@ -39,12 +39,28 @@ export class Controller {
     }
   }
 
-  getData() {
-    this._getParams = `?units=metric&q=${this._cityName}&appid=${this._apiKey}`;
-    let queryCurrentUrl = this._apiCurrentUrl + this._getParams;
-    let queryForecastUrl = this._apiForecastUrl + this._getParams;
-    console.log(`queryCurrentUrl = ${queryCurrentUrl}`);
-    console.log(`queryForecastUrl = ${queryForecastUrl}`);
+  getDataByCity(name) {
+    return this.#getData('q=' + name);
   }
 
+  getDataByCoordinates(latitude, longtitude) {
+    return this.#getData(`lat=${latitude}&lon=${longtitude}`);
+  }
+
+  // private:
+  #apiKey;
+  #apiCurrentUrl;
+  #apiForecastUrl;
+  #getParams;
+
+  #getData(locationParam) {
+    this.#getParams = `?units=metric&${locationParam}&appid=${this.#apiKey}`;
+    
+    let queryCurrentUrl = this.#apiCurrentUrl + this.#getParams;
+    let queryForecastUrl = this.#apiForecastUrl + this.#getParams;
+    console.log(`queryCurrentUrl = ${queryCurrentUrl}`);
+    console.log(`queryForecastUrl = ${queryForecastUrl}`);
+
+    return true;
+  }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,43 +3,68 @@ import { Controller } from './lib.js';
 
 $(document).ready(() => {
 
+  // 0:
   const controller = new Controller();
 
-  $('footer').fadeIn('slow');
-  $('footer > div').show('drop', {direction: 'down'}, 'slow');
+  //->
+  let logo = $('#heading > div > img');
+  let search = $('#search input');
+  let footer = $('footer');
+  let footerContent = $('footer > div');
 
+  // 1:
+  footer.fadeIn('slow');
+  footerContent.show('drop', {direction: 'down'}, 'slow');
+
+  // 2:
   $('#today-tab').click(() => {
     controller.switchTab();
-    $('footer').fadeIn('fast');
-    $('footer > div').show('drop', {direction: 'down'}, 'fast');
+    footer.fadeIn('fast');
+    footerContent.show('drop', {direction: 'down'}, 'fast');
     document.title = 'Triweather - Today';
   });
 
   $('#forecast-tab').click(() => {
     controller.switchTab();
-    $('footer > div').hide('drop', {direction: 'down'}, 'fast');
-    $('footer').fadeOut('fast');
+    footerContent.hide('drop', {direction: 'down'}, 'fast');
+    footer.fadeOut('fast');
     document.title = 'Triweather - 5-day forecast';
   });
 
+  // 3:
   $('#heading').hover(
     function() {
-      $('#heading > div > img').css('transform', 'rotate(270deg)');
+      logo.css('transform', 'rotate(270deg)');
     },
     function() {
-      $('#heading > div > img').css('transform', 'rotate(0deg)');
+      logo.css('transform', 'rotate(0deg)');
     }
   );
 
-  controller.getData();
+  // 4:
+  $('#search').on('submit', (event) => {
+    event.preventDefault();
+    if (search.val()) {
+      if (controller.getDataByCity(search.val())) {
+        search.attr('placeholder', search.val());
+        search.val('');
+        // ...
+      } else {
+        // ...
+      }
+      logo.css('transform', 'rotate(540deg)');
+    }
+  });
+
+  //->
+  controller.getDataByCity('Kyiv');
   
-  if (navigator.geolocation) {
+  //->
+  if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position.coords.latitude);
-        console.log(position.coords.longitude);
+        controller.getDataByCoordinates(position.coords.latitude, position.coords.longitude);
       } 
     );
-  }
 
 });
