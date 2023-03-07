de47e9cef98f739b91a6c0cc35f4c03408bc0810
Parzi41 <95362524+Parzi41@users.noreply.github.com>
2023-03-07 23:06:52 +0200

    Update weather-nearby-renderer.js

--- assets/js/weather-nearby-renderer.js
+++ assets/js/weather-nearby-renderer.js
@@ -2,7 +2,34 @@ export class NearbyWeatherRenderer {
   
   static render(model) {
     let html = `
-      <h1>NEARBY</h1>
+          <h2 style = "color:rgb(200, 50, 50); font-family: Georgia, 'Times New Roman', Times, serif">NEARBY PLACES</h2>
+          <div id = "parent" class = "row">
+
+            <div class = "col-12 col-md-6 col-lg-3" id = "town">
+                <h5 class = "child col-6" style = "color: white;">${model.nearby[0].name}</h5>
+                <img class = "child col-1" src="${model.nearby[0].iconUrl}"></img>
+                <h5 class = "child col-2" style = "color: white;">${model.nearby[0].temp}</h5>
+            </div>
+
+            <div class = "col-12 col-md-6 col-lg-3" id = "town">
+              <h5 class = "child col-6" style = "color: white;">${model.nearby[1].name}</h5>
+              <img class = "child col-1" src="${model.nearby[1].iconUrl}"></img>
+              <h5 class = "child col-2" style = "color: white;">${model.nearby[1].temp}</h5>
+            </div>
+
+            <div class = "col-12 col-md-6 col-lg-3" id = "town">
+              <h5 class = "child col-6" style = "color: white;">${model.nearby[2].name}</h5>
+              <img class = "child col-1" src="${model.nearby[2].iconUrl}"></img>
+              <h5 class = "child col-2" style = "color: white;">${model.nearby[2].temp}</h5>
+            </div>
+
+            <div class = "col-12 col-md-6 col-lg-3" id = "town">
+              <h5 class = "child col-6" style = "color: white;">${model.nearby[3].name}</h5>
+              <img class = "child col-1" src="${model.nearby[3].iconUrl}"></img>
+              <h5 class = "child col-2" style = "color: white;">${model.nearby[3].temp}</h5>
+            </div>
+
+          </div>
     `;
     return html;
   }
